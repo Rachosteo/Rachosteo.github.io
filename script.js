71b46eb3425616d83ad4d0bce36891c0c0213cd2
Craig Brown <craig@Craigs-MBP.home>
2019-04-28 14:23:56 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('#pagepiling').pagepiling({
+        anchors: ["home", "about", "osteo", "treatment", "location", "contact"],
+        sectionsColor: ["#fff", "#5D537F", "#fff", "#fff", "#5D537F", "#fff"],
+        navigation: false,
+        menu: "#nav-menu",
+    });
+});
